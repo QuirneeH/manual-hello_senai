887ac5e81833ee3d33df03bc99a58d7ae7f69d78
Luis Henrique <sathorofire2019@gmail.com>
2023-12-07 21:30:29 -0300

    modified:   public/manual/css/manual.css 	modified:   resources/views/manual/inicio.blade.php 	modified:   resources/views/manual/projeto.blade.php 	modified:   resources/views/manual/usuario.blade.php

--- resources/views/manual/inicio.blade.php
+++ resources/views/manual/inicio.blade.php
@@ -10,18 +10,19 @@
 <p>Seja muito bem-vindo ao Hello_Senai, o seu espaço para compartilhar e interagir em projetos de referentes ao curso Desenvolvimento de Sistemas. Estamos entusiasmados em tê-lo conosco e ansiosos para explorar este ambiente colaborativo de aprendizado e inovação com você.</p><br>
 
 <h1>Por que Usar Hello_Senai?</h1>
-<p>Hello_Senai é mais do que uma plataforma de compartilhamento de projetos; é uma comunidade de estudantes dedicados a aprimorar suas habilidades, colaborar e aprender juntos. Aqui, você terá a oportunidade de criar projetos, compartilhá-los com outros membros do curso de Desenvolvimento de Sistemas e desfrutar de uma jornada de crescimento pessoal e profissional.</p><br>
+<p>Hello_Senai é mais do que uma plataforma de compartilhamento de projetos; é uma comunidade de estudantes dedicados a aprimorar suas habilidades, colaborar e aprender juntos. Aqui, você terá a oportunidade de criar projetos, compartilhá-los com outros membros do curso de Desenvolvimento de Sistemas e desfrutar de uma jornada de crescimento pessoal e profissional.</p>
 
 <p>Crie Projetos Inovadores: Desenvolva vários projetos de desenvolvimento de sistemas do zero ou aprimore projetos existentes. Use nossa plataforma intuitiva para transformar suas ideias em realidade com seus colegas.
     Compartilhe e Colabore: Compartilhe seus projetos com outros alunos e colabore com outros membros. Participe de outros projetos e demonstre suas habilidades para os outros integrantes. Acreditamos que o aprendizado é maximizado quando compartilhado.
 Aprenda e Cresça: Através de interações construtivas, desafios propostos pelos professores e feedback de outros membros, você terá a oportunidade de aprender e aprimorar suas habilidades continuamente.</p><br>
 
 <h1>Como Usar Este Manual?</h1>
-<p>Este manual do usuário é o seu guia completo para navegar e aproveitar ao máximo o Hello_Senai. Ele irá ajudá-lo a compreender todas as funcionalidades, desde a criação de projetos até a colaboração com outros usuários. Dividido em 4 tópicos principais pensados principalmente nos usuários (Aluno e Professor) com características e possibilidades de uso distintas</p><br>
+<p>Este manual do usuário é o seu guia completo para navegar e aproveitar ao máximo o Hello_Senai. Ele irá ajudá-lo a compreender todas as funcionalidades, desde a criação de projetos até a colaboração com outros usuários. Dividido em 4 tópicos principais pensados principalmente nos usuários (Aluno e Professor) com características e possibilidades de uso distintas</p>
 
-<p>Estamos comprometidos em fornecer a você a melhor experiência possível no Hello_Senai. Portanto, não hesite em entrar em contato conosco se tiver dúvidas, sugestões ou problemas. Estamos aqui para ajudar!</p><br>
+<p>Estamos comprometidos em fornecer a você a melhor experiência possível no Hello_Senai. Portanto, não hesite em entrar em contato conosco se tiver dúvidas, sugestões ou problemas. Estamos aqui para ajudar!</p>
 
-<p>Agora, é hora de começar sua jornada de criação e aprendizado. Aproveite ao máximo o Hello_Senai e faça parte de nossa crescente comunidade de entusiastas de desenvolvimento de sistemas. Juntos, podemos alcançar grandes realizações!</p><br>
+<p>Agora, é hora de começar sua jornada de criação e aprendizado. Aproveite ao máximo o Hello_Senai e faça parte de nossa crescente comunidade de entusiastas de desenvolvimento de sistemas. Juntos, podemos alcançar grandes realizações!</p>
+
 <p>Obrigado por escolher o Hello_Senai. Vamos criar, aprender e inovar juntos!.</p>
 @endsection
 <!-- /Conteudo de Inicio -->
--- resources/views/manual/usuario.blade.php
+++ resources/views/manual/usuario.blade.php
@@ -7,7 +7,7 @@
 <!-- Conteudo de Usuario -->
 @section('content')
 <h1>Introdução</h1>
-<p>Para ter acesso à plataforma é primeiramente preciso ser um aluno ou um professor do SENAI da matéria de Desenvolvimento de Sistemas, sendo um aluno vamos ao cadastro.</p><br>
+<p>Para ter acesso à plataforma é primeiramente preciso ser um aluno ou um professor do SENAI da matéria de Desenvolvimento de Sistemas, sendo um aluno vamos ao cadastro.</p>
 
 <ul>
     <li>Para fazer o cadastro na plataforma, primeiro você precisa estar matriculado no SENAI. </li><br>
@@ -31,18 +31,17 @@
     
     <li><h2 id="actions">Acões</h2></li>
     <ol>
-        <p><h3>Criar um Projeto:</h3> Com a permissão do acesso à plataforma o aluno pode criar seus projetos e postar na comunidade. Inicie a página de apresentação de um projeto de desenvolvimento de sistemas e compartilhe sua ideia.</p><br>
+        <p><h3>Criar um Projeto:</h3> Com a permissão do acesso à plataforma o aluno pode criar seus projetos e postar na comunidade. Inicie a página de apresentação de um projeto de desenvolvimento de sistemas e compartilhe sua ideia.</p>
 
-        <p><h3>Editar Projeto:</h3> Faltou algum detalhe? Faça ajustes na página de apresentação do seu projeto, deixando o mesmo melhor apresentável para os visitantes da página do projeto.</p><br>
+        <p><h3>Editar Projeto:</h3> Faltou algum detalhe? Faça ajustes na página de apresentação do seu projeto, deixando o mesmo melhor apresentável para os visitantes da página do projeto.</p>
 
-        <p><h3>Comentar nos Projetos:</h3> Interaja com outros alunos deixando comentários em projetos da comunidade, você pode ter pensando em algum detalhe que outras pessoas não viram. Lembre-se, respeite tanto o projeto quanto os alunos da página, exceder o limite será sujeito a punição.</p><br>
+        <p><h3>Comentar nos Projetos:</h3> Interaja com outros alunos deixando comentários em projetos da comunidade, você pode ter pensando em algum detalhe que outras pessoas não viram. Lembre-se, respeite tanto o projeto quanto os alunos da página, exceder o limite será sujeito a punição.</p>
 
-        <p><h3>Criar um Convite:</h3> Crie convites para seus projetos para montar equipes da comunidade de alunos colaborarem nos mesmos. Dê uma breve descrição sobre o projeto e o mesmo anexado para que outros alunos possam visualizar com mais detalhes, especifique quais habilidades de programação a sua equipe está necessitando para que alunos com essas habilidades entrem em contato com você deixando seu e-mail de contato.</p><br>
+        <p><h3>Criar um Convite:</h3> Crie convites para seus projetos para montar equipes da comunidade de alunos colaborarem nos mesmos. Dê uma breve descrição sobre o projeto e o mesmo anexado para que outros alunos possam visualizar com mais detalhes, especifique quais habilidades de programação a sua equipe está necessitando para que alunos com essas habilidades entrem em contato com você deixando seu e-mail de contato.</p>
 
-        <p><h3>Vincular Convites:</h3> Conecte seus projetos a convites e deixe que outros alunos vejam e se interessem a participar do seu Projeto. Alunos interessados em participar enviaram o e-mail pedindo a participação dele no seu projeto, verifique o seu e-mail para a montar a sua equipe.</p><br>
+        <p><h3>Vincular Convites:</h3> Conecte seus projetos a convites e deixe que outros alunos vejam e se interessem a participar do seu Projeto. Alunos interessados em participar enviaram o e-mail pedindo a participação dele no seu projeto, verifique o seu e-mail para a montar a sua equipe.</p>
 
-
-        <p><h3>Pedir Participação em Outros Projetos:</h3> Envie solicitações para participar de projetos de outros alunos enviando um e-mail para os responsáveis pelo projeto em questão e dividam experiências demonstrando suas habilidades de programador.</p><br>
+        <p><h3>Pedir Participação em Outros Projetos:</h3> Envie solicitações para participar de projetos de outros alunos enviando um e-mail para os responsáveis pelo projeto em questão e dividam experiências demonstrando suas habilidades de programador.</p>
     </ol>
 </ol>
 
@@ -50,12 +49,12 @@
 <ol>
     <li><h2 id="profile_P">Perfil</h2></li>
     <ol>
-        <p><h3>Visualização:</h3> Visualize seu perfil e informações pessoais. Aqui pode ser visualizado suas informações pessoais e profissionais por seus alunos e outros professores sobre você.</p><br>
+        <p><h3>Visualização:</h3> Visualize seu perfil e informações pessoais. Aqui pode ser visualizado suas informações pessoais e profissionais por seus alunos e outros professores sobre você.</p>
         
-        <p><h3>Editar Perfil:</h3> Atualize suas informações de perfil, incluindo --Campos do passo a passo--. deixe-o melhor apresentável aos visitantes ou deixe detalhes sobre você aqui para que lhe conheçam.</p><br>
+        <p><h3>Editar Perfil:</h3> Atualize suas informações de perfil, incluindo --Campos do passo a passo--. deixe-o melhor apresentável aos visitantes ou deixe detalhes sobre você aqui para que lhe conheçam.</p>
     </ol>
     <li><h2 id="challenger">Desafio</h2></li>
-    <p>Os professores têm um papel importante no aprendizado do aluno, além de passar conhecimento e dividir experiências de trabalho, estimula com atividades em sala de aula ou para casa, tendo isso em mente, na plataforma os professores podem propor desafios aos alunos do curso de desenvolvimento de sistemas, podendo ver as proposta dos alunos, ter acesso ao diretório.</p><br>
+    <p>Os professores têm um papel importante no aprendizado do aluno, além de passar conhecimento e dividir experiências de trabalho, estimula com atividades em sala de aula ou para casa, tendo isso em mente, na plataforma os professores podem propor desafios aos alunos do curso de desenvolvimento de sistemas, podendo ver as proposta dos alunos, ter acesso ao diretório.</p>
     
     <ol>
         <p><h3>Criar um Desafio:</h3> Abra um desafio para os alunos de Desenvolvimento de Sistemas com problemas existentes, ideias mirabolantes ou para testar os conhecimentos dos alunos do curso.</p><br>
