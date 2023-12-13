1117c2c938a4717260aa6d9c22fafa2b9e5d2230
Luis Henrique <sathorofire2019@gmail.com>
2023-12-12 21:23:44 -0300

    modified:   public/manual/css/manual.css 	modified:   public/manual/css/root.css 	modified:   resources/views/manual/usuario.blade.php

--- resources/views/manual/usuario.blade.php
+++ resources/views/manual/usuario.blade.php
@@ -10,17 +10,20 @@
 <p>Para ter acesso à plataforma é primeiramente preciso ser um aluno ou um professor do SENAI da matéria de Desenvolvimento de Sistemas, sendo um aluno vamos ao cadastro.</p>
 
 <ul>
-    <span>Para fazer o cadastro na plataforma, primeiro você precisa estar matriculado no SENAI.</span><br><br>
+    <div class="alert">
+        <p>Para fazer o cadastro na plataforma, primeiro você precisa estar matriculado no SENAI.</p>
+    </div>
+
     <div class="tutorial">
         <div id="image">
-            <li>Na primeira tela, escolha aluno, caso você seja um aluno da instituição ou professor caso você seja um professor da instituição.</li>
+            <li>Na primeira tela, escolha “Sou aluno”, caso você seja um aluno da instituição ou professor caso você seja um professor da instituição.</li>
             <img src="https://t4.ftcdn.net/jpg/02/00/68/69/360_F_200686969_GJ7zbz2qaNIE4dyHSbZkQXvNPzRuwlr3.jpg" alt="">
         </div>
     </div>
 
     <div class="tutorial">
         <div id="image">
-            <li>Clique na opção de se registrar, para fazer o cadastro na plataforma</li>
+            <li>Clique na opção “Não tem uma conta?”</li>
             <img src="https://t4.ftcdn.net/jpg/02/00/68/69/360_F_200686969_GJ7zbz2qaNIE4dyHSbZkQXvNPzRuwlr3.jpg" alt="">
         </div>
     </div>
@@ -31,20 +34,47 @@
             <img src="https://t4.ftcdn.net/jpg/02/00/68/69/360_F_200686969_GJ7zbz2qaNIE4dyHSbZkQXvNPzRuwlr3.jpg" alt="">
         </div>
     </div>
-    
 </ul><br>
 
+<p>Com seu perfil criado seu acesso a plataforma agora foi aceito. <b>Bem vindo!</b>.</p><br>
+
 <h1 id="user_A">Usuário Aluno</h1>
-<p>Com seu perfil criado seu acesso a plataforma agora foi aceito. Bem vindo.</p>
-<p>Ao abrir a plataforma pela primeira vez --(Nome da tela após o login / cadastro)-- você consegue acessar seu perfil clicando em --(“...”)-- será redirecionamento para a sua tela de perfil.</p><br>
+<p>Ao abrir a plataforma sendo um aluno já cadastrado na plataforma você consegue acessar seu perfil nesse seguinte passo a passo:</p>
+
+<ul>
+    <div class="tutorial">
+        <div id="image">
+            <li>Clique em “Sou Aluno”.</li>
+            <img src="https://t4.ftcdn.net/jpg/02/00/68/69/360_F_200686969_GJ7zbz2qaNIE4dyHSbZkQXvNPzRuwlr3.jpg" alt="">
+        </div>
+    </div>
+
+    <div class="tutorial">
+        <div id="image">
+            <li>Digite seu Email de Aluno fornecido pelo SENAI e sua Senha criada anteriormente.</li>
+            <img src="https://t4.ftcdn.net/jpg/02/00/68/69/360_F_200686969_GJ7zbz2qaNIE4dyHSbZkQXvNPzRuwlr3.jpg" alt="">
+        </div>
+    </div>
+
+    <div class="tutorial">
+        <div id="image">
+            <li>Clique no botão “Log In”</li>
+            <img src="https://t4.ftcdn.net/jpg/02/00/68/69/360_F_200686969_GJ7zbz2qaNIE4dyHSbZkQXvNPzRuwlr3.jpg" alt="">
+        </div>
+    </div>
+
+    <div class="error">
+        <p><b>Erro de Acesso à sua Conta:</b> Se você encontrar dificuldades para acessar seu perfil após o cadastro, verifique se inseriu corretamente as informações de login e senha. Se o problema persistir, clique em "Recuperar Senha" para redefinir sua senha e tente novamente. Se o problema continuar, entre em contato com o suporte.</p>
+    </div>
+</ul><br>
 
 <ol>
     <li><h2 id="profile_A">Perfil</h2></li>
     <ol>
         <p><h3>Visualização:</h3> Visualize seu perfil com informações preenchidas anteriormente. --Campos do passo a passo--
-        Aqui ficam as informações sobre você aluno onde outros usuários podem acessar e visualizar seus projetos, lhe conhecer melhor e ter acesso mais direcionado a sua pessoa.</p><br>
+        Aqui ficam as informações sobre você aluno onde outros usuários podem acessar e visualizar seus projetos, lhe conhecer melhor e ter acesso mais direcionado a sua pessoa.</p>
 
-        <p><h3>Editar Perfil:</h3> Atualize suas informações de perfil, incluindo nome, foto e outras configurações.</p><br>
+        <p><h3>Editar Perfil:</h3> Atualize suas informações de perfil, incluindo nome, foto e outras configurações.</p>
     </ol>
     
     <li><h2 id="actions">Acões</h2></li>
@@ -75,11 +105,11 @@
     <p>Os professores têm um papel importante no aprendizado do aluno, além de passar conhecimento e dividir experiências de trabalho, estimula com atividades em sala de aula ou para casa, tendo isso em mente, na plataforma os professores podem propor desafios aos alunos do curso de desenvolvimento de sistemas, podendo ver as proposta dos alunos, ter acesso ao diretório.</p>
     
     <ol>
-        <p><h3>Criar um Desafio:</h3> Abra um desafio para os alunos de Desenvolvimento de Sistemas com problemas existentes, ideias mirabolantes ou para testar os conhecimentos dos alunos do curso.</p><br>
+        <p><h3>Criar um Desafio:</h3> Abra um desafio para os alunos de Desenvolvimento de Sistemas com problemas existentes, ideias mirabolantes ou para testar os conhecimentos dos alunos do curso.</p>
         
-        <p><h3>Visualizar Desafio:</h3> Com o desafio criado, ele pode ser visualizado tanto pelos professores quanto pelos alunos, porém, as postagens são feitas apenas pelos alunos.</p><br>
+        <p><h3>Visualizar Desafio:</h3> Com o desafio criado, ele pode ser visualizado tanto pelos professores quanto pelos alunos, porém, as postagens são feitas apenas pelos alunos.</p>
 
-        <p><h3>Editar Desafio:</h3> Faltou alguma informação importante ou deseja mudar a proposta? Edite os campos para maior clareza das informações do desafio para os seus alunos.</p><br>
+        <p><h3>Editar Desafio:</h3> Faltou alguma informação importante ou deseja mudar a proposta? Edite os campos para maior clareza das informações do desafio para os seus alunos.</p>
 
         <p><h3>Visualizar Projetos:</h3> Os Projetos postados pelos alunos podem ser visualizados com mais detalhes ao clicar no mesmo, sendo redirecionado para uma página do Projeto em específico, onde possam ler e comentar novas ideias.</p>
     </ol>
